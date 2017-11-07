5fa3000bb9708817ee60e4823c27d8e358ca7e55
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 11:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
